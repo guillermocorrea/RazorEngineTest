ab37451e8b55f46e9edd9fa634f3566a0216ea25
Guillermo Correa <guille_freelance@hotmail.com>
2012-08-14 21:08:52 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<h2>RazorEngineTest</h2>
+<p>c# test project that shows the use of RazorEngine outside MVC.</p>
---
+++ RazorEngineTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RazorEngine;
+using System.IO;
+
+namespace RazorEngineTest
+{
+    class Program
+    {
+        public static string getTemplate()
+        {
+            var model = new EmailModel { Name = "World", Email = "someone@somewhere.com" };
+            string template = File.OpenText("templates/template.cshtml").ReadToEnd();
+            Razor.Compile<EmailModel>(template, "complex");
+            return Razor.Parse(template, model);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getTemplate());
+            Console.ReadLine();
+        }
+    }
+
+    public class EmailModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
